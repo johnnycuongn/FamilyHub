842bb808a831afd6695d81b0600e5a8e936fe6c9
Johnny Nguyen <cuongdn2001@gmail.com>
2023-09-05 15:53:16 +1000

    Initial Commit

---
+++ iOSFamilyHub/iOSFamilyHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSFamilyHubApp.swift
+//  iOSFamilyHub
+//
+//  Created by Johnny on 5/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct iOSFamilyHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
